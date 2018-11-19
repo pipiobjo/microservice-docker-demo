12ee27bf9af84d401c0cd2ad4838ffdadd4b8d03
Björn <pipiobjo@googlemail.com>
2018-11-19 20:45:29 +0100

    Add documentation for learning steaps

--- backend-services/helloWorldApp/src/main/java/com/pipiobjo/microservice/helloWorldApp/services/GreetingService.java
+++ backend-services/helloWorldApp/src/main/java/com/pipiobjo/microservice/helloWorldApp/services/GreetingService.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @Component
 public class GreetingService {
     public static final String GREETING_DEFAULT_VALUE = "World";
-    public static final String template = "Hello, %s!";
+    public static final String template = "Hello1, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     public Greeting buildGreeting(String name){
