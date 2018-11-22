36b55b9c804b19ea25baf7052a5c770bde6cfd29
Björn <pipiobjo@googlemail.com>
2018-11-22 15:43:59 +0100

    add lombok config to ignore generated methods during jacocoo coverage analysis

--- backend-services/helloWorldApp/src/main/java/com/pipiobjo/microservice/helloworldapp/services/GreetingService.java
+++ backend-services/helloWorldApp/src/main/java/com/pipiobjo/microservice/helloworldapp/services/GreetingService.java
@@ -9,13 +9,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @Component
 public class GreetingService {
     public static final String GREETING_DEFAULT_VALUE = "World";
-    public static final String template = "Hello1, %s!";
+    public static final String TEMPLATE = "Hello1, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     public Greeting buildGreeting(String name){
         if(StringUtils.isBlank(name)){
             name = GREETING_DEFAULT_VALUE;
         }
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 }
