fdd161a418b67348bb625c7c53dc8fc1e919056c
Björn <pipiobjo@googlemail.com>
2018-11-22 20:54:11 +0100

    Adding eureka for service discovery and docker compose for local orchestration

--- backend-services/motdApp/src/main/java/com/pipiobjo/microservice/motdapp/MainApplication.java
+++ backend-services/motdApp/src/main/java/com/pipiobjo/microservice/motdapp/MainApplication.java
@@ -3,11 +3,13 @@ package com.pipiobjo.microservice.motdapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableDiscoveryClient
 public class MainApplication {
 
 	public static void main(String[] args) {
